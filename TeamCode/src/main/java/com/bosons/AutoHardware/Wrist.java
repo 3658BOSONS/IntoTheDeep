13db7d2c06cde8046f1fabc0deb773c57910f58e
Carl <carlankorhillam@gmail.com>
2024-11-20 09:31:35 -0600

    added wrist class

---
+++ TeamCode/src/main/java/com/bosons/AutoHardware/Wrist.java
@@ -0,0 +1,63 @@
+package com.bosons.AutoHardware;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Wrist {
+    public Servo wristServo;
+    public Wrist(HardwareMap hwm){
+        wristServo = hwm.get(Servo.class,"wrist");
+    }
+
+    public class intake implements Action{
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double targetPos = 90.0;
+            wristServo.setPosition(targetPos);
+            double currentPos = wristServo.getPosition();
+            packet.put("Wrist | targetPos: ", targetPos);
+            packet.put("Wrist | currentPos: ", currentPos);
+            return !(currentPos >= targetPos);
+        }
+    }
+
+    public Action intake(){
+        return new intake();
+    }
+
+    public class standby implements Action{
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double targetPos = 0.0;
+            wristServo.setPosition(targetPos);
+            double currentPos = wristServo.getPosition();
+            packet.put("Wrist | targetPos: ", targetPos);
+            packet.put("Wrist | currentPos: ", currentPos);
+            return !(currentPos >= targetPos);
+        }
+    }
+
+    public Action standby(){
+        return new standby();
+    }
+
+    public class bucket implements Action{
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double targetPos = 0.0;
+            wristServo.setPosition(targetPos);
+            double currentPos = wristServo.getPosition();
+            packet.put("Wrist | targetPos: ", targetPos);
+            packet.put("Wrist | currentPos: ", currentPos);
+            return !(currentPos >= targetPos);
+        }
+    }
+
+    public Action bucket(){
+        return new bucket();
+    }
+}
